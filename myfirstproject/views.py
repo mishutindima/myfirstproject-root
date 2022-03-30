d82b5d874158ee6693522629ff6e64a94d4f1c6c
Мишутин Дмитрий <Dmitry.Mishutin@inside.glowbyte.com>
2022-03-30 09:59:09 +0300

    Add count feature

--- myfirstproject/views.py
+++ myfirstproject/views.py
@@ -10,4 +10,5 @@ def home(request):
 def reverse(request):
     user_text = request.GET["usertext"]
     reversed_text = user_text[::-1]
-    return render(request, "reverse.html", {"usertext":user_text, "reversedtext":reversed_text})
+    count_of_words = len(user_text.split (" "))
+    return render(request, "reverse.html", {"usertext":user_text, "reversedtext":reversed_text, "countofwords":count_of_words})
